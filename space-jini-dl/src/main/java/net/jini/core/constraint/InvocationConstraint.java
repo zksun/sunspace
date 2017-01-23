e4feefba173fe732d6d77d1d25e25446f1bea9d6
缉熙 <zhikun.szk@alibaba-inc.com>
2017-01-23 23:26:56 +0800

    add space-core module

--- space-jini-dl/src/main/java/net/jini/core/constraint/InvocationConstraint.java
+++
@@ -1,7 +0,0 @@
-package net.jini.core.constraint;
-
-/**
- * Created by zksun on 23/01/2017.
- */
-public interface InvocationConstraint {
-}
--- space-jini-dl/src/main/java/net/jini/core/constraint/RelativeTimeConstraint.java
+++
@@ -1,8 +0,0 @@
-package net.jini.core.constraint;
-
-/**
- * Created by zksun on 23/01/2017.
- */
-public interface RelativeTimeConstraint extends InvocationConstraint {
-    InvocationConstraint makeAbsolute(long baseTime);
-}
