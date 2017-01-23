97f2c99d15240638442c78ca89191e07090238eb
缉熙 <zhikun.szk@alibaba-inc.com>
2017-01-23 15:09:26 +0800

    add space-core module

--- space-jini/src/main/java/net/jini/core/constraint/ClientMaxPrincipal.java
+++ space-jini/src/main/java/net/jini/core/constraint/ClientMaxPrincipal.java
@@ -1,11 +1,13 @@
 package net.jini.core.constraint;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.Serializable;
 import java.security.Principal;
+import java.util.Collection;
 
 /**
- * Created by hanshou on 23/01/2017.
+ * Created by zksun on 23/01/2017.
  */
 public final class ClientMaxPrincipal implements InvocationConstraint, Serializable {
 
@@ -18,11 +20,32 @@ public final class ClientMaxPrincipal implements InvocationConstraint, Serializa
         this.principals = new Principal[]{principal};
     }
 
-    public ClientMaxPrincipal(Principal[] principal) {
-        this.principals = new Principal[]{principal};
+    public ClientMaxPrincipal(Principal[] principals) {
+        this.principals = Constraint.reduce(principals);
     }
 
-    private void readObject() throws IOException, ClassNotFoundException {
+    public ClientMaxPrincipal(Collection c) {
+        principals = Constraint.reduce(c);
+    }
 
+
+    @Override
+    public int hashCode() {
+        return (ClientMaxPrincipal.class.hashCode() + Constraint.hash(principals));
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return (obj instanceof ClientMaxPrincipal && Constraint.equal(principals, ((ClientMaxPrincipal) obj).principals));
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    private void readObject(ObjectInputStream objectInputStream) throws IOException, ClassNotFoundException {
+        objectInputStream.defaultReadObject();
+        Constraint.verify(principals);
     }
 }
